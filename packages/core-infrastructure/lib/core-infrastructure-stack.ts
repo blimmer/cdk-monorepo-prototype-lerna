a0f77140de8a636ec3188679a6656d0315fdfe3b
Ben Limmer <ben@benlimmer.com>
2021-10-13 11:00:30 -0600

    make pkg1 dependent on core infra

--- packages/core-infrastructure/lib/core-infrastructure-stack.ts
+++ packages/core-infrastructure/lib/core-infrastructure-stack.ts
@@ -1,8 +1,8 @@
-import * as cdk from '@aws-cdk/core';
-import * as sns from '@aws-cdk/aws-sns'
+import * as cdk from "@aws-cdk/core";
+import * as sns from "@aws-cdk/aws-sns";
 
 export class CoreInfrastructureStack extends cdk.Stack {
-  readonly topic = new sns.Topic(this, 'Topic');
+  readonly topic = new sns.Topic(this, "Topic1");
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
