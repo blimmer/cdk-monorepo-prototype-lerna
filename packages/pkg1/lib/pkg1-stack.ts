3cf1de5295fe94c618f35c9c654f27b0b2c2e6ad
Ben Limmer <ben@benlimmer.com>
2021-10-13 10:32:16 -0600

    remove lib/ path specification

--- packages/pkg1/lib/pkg1-stack.ts
+++ packages/pkg1/lib/pkg1-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as nodelambda from "@aws-cdk/aws-lambda-nodejs";
 import { join } from "path";
-import { getSomething } from "core-infrastructure/lib";
+import { getSomething } from "core-infrastructure";
 
 export class Pkg1Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
