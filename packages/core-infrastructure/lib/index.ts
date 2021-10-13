26d7de55951e29d450899a9db9141655e51b1065
Ben Limmer <ben@benlimmer.com>
2021-10-13 10:31:16 -0600

    initially working tsconfig setup

--- packages/core-infrastructure/lib/index.ts
+++ packages/core-infrastructure/lib/index.ts
@@ -2,5 +2,5 @@ import * as cdk from '@aws-cdk/core'
 import * as sns from '@aws-cdk/aws-sns'
 
 export function getSomething(scope: cdk.Construct) {
-  return sns.Topic.fromTopicArn(scope, 'foo', 'arn:aws:foo')
+  return sns.Topic.fromTopicArn(scope, 'foo', 'arn:aws:sns:us-east-2:444455556666:MyTopic')
 }
--- packages/pkg1/lib/pkg1-stack.ts
+++ packages/pkg1/lib/pkg1-stack.ts
@@ -1,14 +1,18 @@
-import * as cdk from '@aws-cdk/core';
-import * as nodelambda from '@aws-cdk/aws-lambda-nodejs'
-import { join } from 'path'
+import * as cdk from "@aws-cdk/core";
+import * as nodelambda from "@aws-cdk/aws-lambda-nodejs";
+import { join } from "path";
+import { getSomething } from "core-infrastructure/lib";
 
 export class Pkg1Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const entry = join(__dirname, 'lambda', 'index.ts')
-    new nodelambda.NodejsFunction(this, 'Lambda', {
+    const entry = join(__dirname, "lambda", "index.ts");
+    new nodelambda.NodejsFunction(this, "Lambda", {
       entry,
-    })
+      environment: {
+        SNS_TOPIC: getSomething(this).topicArn,
+      },
+    });
   }
 }
