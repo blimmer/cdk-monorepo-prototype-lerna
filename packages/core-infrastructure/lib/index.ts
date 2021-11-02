55f13b5d05f1adca7fde930ac44350b129096d6e
Ben Limmer <ben@benlimmer.com>
2021-11-02 11:33:30 -0600

    feat: another update to core-infra

--- packages/core-infrastructure/lib/index.ts
+++ packages/core-infrastructure/lib/index.ts
@@ -2,5 +2,5 @@ import * as cdk from "@aws-cdk/core";
 import * as sns from "@aws-cdk/aws-sns";
 
 export function getSomething(scope: cdk.Construct) {
-  return sns.Topic.fromTopicArn(scope, "Change1", "arn:aws:sns:us-east-2:444455556666:MyTopic2");
+  return sns.Topic.fromTopicArn(scope, "Change2", "arn:aws:sns:us-east-2:444455556666:MyTopic2");
 }
